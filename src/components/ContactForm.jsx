ce3676be9032739fd2fa474956b52a5b5a28f513
eik xheleshi <erikxheleshi@gmail.com>
2024-02-15 21:43:23 +0100

    contact form is fully fuctional

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,39 +1,41 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import Button from './Button';
+import emailjs from '@emailjs/browser';
 
 const ContactForm = () => {
-  const [formData, setFormData] = useState({
-    name: '',
-    email: '',
-    message: '',
-  });
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({ ...formData, [name]: value });
-  };
+  const form = useRef();
 
-  const handleSubmit = (e) => {
+  const sendEmail = (e) => {
     e.preventDefault();
-    console.log('Form submitted:', formData);
+
+    emailjs
+      .sendForm('service_yxnzsks', 'template_xsaahgu', form.current, {
+        publicKey: 'udUo5j6YppzkUAZpr',
+      })
+      .then(
+        () => {
+          console.log('SUCCESS!');
+        },
+        (error) => {
+          console.log('FAILED...', error.text);
+        },
+      );
   };
 
   return (
     <div className='w-full'>
 
         <div className="text-center">
-                  <h2 className='text-2xl'>Contact me</h2>
+        <h2 className='text-2xl'>Contact me</h2>
         </div>
 
-      <form onSubmit={handleSubmit}>
+      <form ref={form} onSubmit={sendEmail}>
         <div className='flex flex-col py-4 gap-4'>
           <label htmlFor="name">Name:</label>
           <input
             type="text"
-            id="name"
-            name="name"
-            value={formData.name}
-            onChange={handleChange}
+            name="user_name"
             required
           />
         </div>
@@ -42,10 +44,7 @@ const ContactForm = () => {
           <label htmlFor="email">Email:</label>
           <input
             type="email"
-            id="email"
-            name="email"
-            value={formData.email}
-            onChange={handleChange}
+            name="user_email"
             required
           />
         </div>
@@ -53,18 +52,16 @@ const ContactForm = () => {
         <div className='flex flex-col py-4 gap-4'>
           <label htmlFor="message">Message:</label>
           <textarea
-            id="message"
-            name="message"
-            value={formData.message}
-            onChange={handleChange}
-            maxLength={500}
+             name="message"
+            // maxLength={500}
             required
           />
-          <p>Characters left: {500 - formData.message.length}</p>
+          {/* <p>Characters left: {500 - formData.message.length}</p> */}
         </div>
 
         <div className="py-4">
           <Button 
+          value="send"
           type="submit"
           btnName="submit"/>
         </div>
